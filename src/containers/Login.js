a3dfe0278f7abc861bb3f73e37a913c7d8800b94
showeimer <showeimer@gmail.com>
2017-09-04 19:25:57 -0400

    created Login container, mapped login action to it

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,42 +6,33 @@ class Register extends Component {
   constructor(props) {
     super(props);
 
-    this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSecretChange = this.handleSecretChange.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
 
     this.state = {
-      name: '',
       email: '',
       password: '',
-      secret: ''
     }
   }
 
-  handleNameChange(event) {
-    this.setState({name: event.target.value})
-  }
+
   handleEmailChange(event) {
     this.setState({email: event.target.value})
   }
   handlePasswordChange(event) {
     this.setState({password: event.target.value})
   }
-  handleSecretChange(event) {
-    this.setState({secret: event.target.value})
-  }
-  handleRegisterSubmit(event) {
+
+  handleLoginSubmit(event) {
     event.preventDefault();
-    register(this.state.name, this.state.email, this.state.password, this.state.secret);
-    this.setState({name: '', email: '', password: '', secret: ''});
+    login(this.state.email,this.state.password);
+    this.setState({email: '', password: ''});
   }
 
   render() {
     return (
-      <form onSubmit={this.handleRegisterSubmit}>
-        <label>Name:</label>
-        <input value={this.state.name} onChange={this.handleNameChange} placeholder="Full Name" name="name" type="text" />
+      <form onSubmit={this.handleLoginSubmit}>
 
         <label>E-mail:</label>
         <input value={this.state.email} onChange={this.handleEmailChange} placeholder="Email address" name="email" type="email" />
@@ -49,10 +40,7 @@ class Register extends Component {
         <label>Password:</label>
         <input value={this.state.password} onChange={this.handlePasswordChange} placeholder="Enter a password" name="password" type="password" />
 
-        <label>Your Secret:</label>
-        <textarea value={this.state.secret} onChange={this.handleSecretChange} type="text" name="secret" />
-
-        <button className="btn">Submit</button>
+        <button className="btn">Login</button>
       </form>
     )
   }
@@ -60,8 +48,8 @@ class Register extends Component {
 
 const mapDispatchToProps = function(dispatch) {
     return {
-        register: function(name, email, password, secret) {
-            dispatch(register(name, email, password, secret));
+        login: function(email, password) {
+            dispatch(register(email, password));
         }
     }
 }
